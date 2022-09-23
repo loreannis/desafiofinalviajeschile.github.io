f4a2215b36fff2a9edc2573570851636a3bc9a2c
loreannis <leishka26@gmail.com>
2022-09-22 22:21:52 -0300

    Tuve un leve problema en el git guardo de nuevo

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,14 @@ $(".card2").click(function(){
 //Recordatorio de escribir los datos de los formularios y envio del mensaje
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+
+//Hover Titulo card "segundo bloque"
+$(document).ready(function(){
+    $(".tituloscard").hover(function(){
+        $(this).css({
+            "color":"gray",
+        })
+    })
+})
